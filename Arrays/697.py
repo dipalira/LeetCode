9c7349d0ad867e0d876bc17e5e035536bba4b72d
dipalira <dipaliranjan@gmail.com>
2018-06-04 22:49:21 -0500

    5

--- Arrays/697.py
+++ Arrays/697.py
@@ -1,4 +1,26 @@
-nums = [1,2,2,3,1,4,2]
-dic ={}
-for i,n in enumerate(nums):
-	
+class Solution:
+    def findShortestSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dic ={}
+
+        freq = 0
+        item = 0
+        if len(nums) == 1:
+            return 1
+        for i,n in enumerate(nums):
+            if n in dic:
+                dic[n].append(i)
+
+            else:
+                dic[n] = [i]
+            if freq < len(dic[n]):
+                freq = len(dic[n])
+                item = n
+            elif freq == len(dic[n]):
+                if dic[n][-1] - dic[n][0] +1 < dic[item][-1] - dic[item][0] +1:
+                    freq = len(dic[n])
+                    item = n
+        return dic[item][-1] - dic[item][0] +1
