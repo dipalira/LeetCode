cc01197870352e6969769d8782e9c01f2ab17e62
dipalira <dipaliranjan@gmail.com>
2018-06-22 09:15:38 -0500

    121

--- Arrays/121.py
+++ Arrays/121.py
@@ -1,6 +1,19 @@
-prices = [7,1,5,3,6,4]
-profit = 0
-for i in range(1,len(prices)) :
-	if prices[i] - prices[i-1] > profit:
-		profit = prices[i] - prices[i-1]
-print(profit)
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+       
+        if len(prices) == 0:
+            return 0
+        min_ = prices[0]
+        profit = 0
+        min_i = 0
+        for i,p in enumerate(prices):
+
+            if min_ > p :
+                min_ = p
+            elif p - min_ > profit :
+                profit = p - min_
+        return profit
