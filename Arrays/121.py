d16021613c6da3d9f3b0b163f9bd9e24369faf35
dipalira <dipaliranjan@gmail.com>
2018-06-23 12:41:07 -0500

    Merge branch 'master' of https://github.com/dipalira/LeetCode

--- Arrays/121.py
+++ Arrays/121.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 prices = [7,6,4,3,1]
 profit = 0
 max_,min_,max_i,min_i = 0,prices[0],0,0
@@ -12,3 +13,24 @@ for i,p in enumerate(prices):
 if min_ > max_:
 	min_ = 0
 print(max_, min_)
+=======
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+       
+        if len(prices) == 0:
+            return 0
+        min_ = prices[0]
+        profit = 0
+        min_i = 0
+        for i,p in enumerate(prices):
+
+            if min_ > p :
+                min_ = p
+            elif p - min_ > profit :
+                profit = p - min_
+        return profit
+>>>>>>> 1f20a865c68db1f08f2b0e496f75d24f246a26e7
