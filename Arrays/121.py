eef22cb199507a94ecb5f878a99d7b7fe9c043be
dipalira <dipaliranjan@gmail.com>
2018-06-26 20:38:41 -0500

    new

--- Arrays/121.py
+++ Arrays/121.py
@@ -1,36 +1,6 @@
-<<<<<<< HEAD
 prices = [7,6,4,3,1]
 profit = 0
-max_,min_,max_i,min_i = 0,prices[0],0,0
+min_ = 0
 for i,p in enumerate(prices):
-	if  max_ < p and i >max_i and i > min_i:
-		max_ = p
-		max_i = i
-	if min_i <i and min_ > p :
-		print(p)
-		min_ = p
-		min_i = i
-if min_ > max_:
-	min_ = 0
+	profit = p - 
 print(max_, min_)
-=======
-class Solution:
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-       
-        if len(prices) == 0:
-            return 0
-        min_ = prices[0]
-        profit = 0
-        min_i = 0
-        for i,p in enumerate(prices):
-
-            if min_ > p :
-                min_ = p
-            elif p - min_ > profit :
-                profit = p - min_
-        return profit
->>>>>>> 1f20a865c68db1f08f2b0e496f75d24f246a26e7
---
+++ Arrays/35.py
@@ -0,0 +1,18 @@
+nums = [1,3,5,6]
+target = 7
+def binary_search(array, target):
+    lower = 0
+    upper = len(array)
+    while lower < upper:   # use < instead of <=
+        x = lower + (upper - lower) // 2
+        val = array[x]
+        if target == val:
+            return x
+        elif target > val:
+            if lower == x:   # these two are the actual lines
+                break        # you're looking for
+            lower = x
+        elif target < val:
+            upper = x
+    print(lower,upper)
+binary_search(nums,target)
