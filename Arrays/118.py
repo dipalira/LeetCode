18f046992f2de7217452245744431670fb2c11ce
dipalira <dipaliranjan@gmail.com>
2018-06-26 19:54:45 -0500

    `118

--- Arrays/118.py
+++ Arrays/118.py
@@ -1,3 +1,12 @@
-numRows = 3
+numRows = 6
 output = [[1]*i for i in range(1, numRows+1 )]
+print(output)
+for row_ in range(2,len(output)):
+	row = output[row_]
+	for i in range(1,len(row)-1):
+		try:
+			output[row_][i] = output[row_-1][i] + output[row_-1][i-1]
+
+		except IndexError:
+			continue
 print(output)
