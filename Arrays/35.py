0e7126c70972dfc6aa0dab68db5223921a15bc93
dipalira <dipaliranjan@gmail.com>
2018-06-29 08:38:11 -0500

    Update 35.py

--- Arrays/35.py
+++ Arrays/35.py
@@ -1,18 +1,13 @@
-nums = [1,3,5,6]
-target = 7
-def binary_search(array, target):
-    lower = 0
-    upper = len(array)
-    while lower < upper:   # use < instead of <=
-        x = lower + (upper - lower) // 2
-        val = array[x]
-        if target == val:
-            return x
-        elif target > val:
-            if lower == x:   # these two are the actual lines
-                break        # you're looking for
-            lower = x
-        elif target < val:
-            upper = x
-    print(lower,upper)
-binary_search(nums,target)
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        index_ = 0
+        
+        for num in nums:
+            if num < target:
+                index_ += 1
+        return index_
