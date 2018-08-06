3aa2f6d73ea42c8e8a11ae7f34a88270116e5ac5
dipalira <dipaliranjan@gmail.com>
2018-08-05 20:45:50 -0500

    e

---
+++ Arrays/Twp Pointer Problem Sample.py
@@ -0,0 +1,20 @@
+"""
+Given a sorted array A, having N integers. You need to find any pair(i,j) having sum as given number X.
+
+"""
+
+A = [1,3,4,6,8,11]
+X = 10 
+# we keep two pointers, one at the starting tip of the array and another at the tail.
+i,j =0, len(A)-1
+sum_ = 0
+while (i < j):
+	sum_ = A[j] + A[i]
+	print(A[i],A[j], A[i] + A[j])
+	if sum_ == X:
+		print(A[i],A[j])
+		break
+	elif sum_  > X:
+		j-=1
+	else:
+		i+=1
