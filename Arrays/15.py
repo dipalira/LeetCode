9483e7351b21de284bf3dde9b29928d0c35354c2
dipalira <dipaliranjan@gmail.com>
2018-08-09 00:06:57 -0500

    r

--- Arrays/15.py
+++ Arrays/15.py
@@ -1,22 +1,34 @@
-nums =  [-1, 0, 1, 2, -1, -4]
+nums =  [1,2,-2,-1]
 def threeSum( nums):
-    """
-    :type nums: List[int]
-    :rtype: List[List[int]]
-    """
-    nums.sort()
-    curr_2 = 0
-    curr_3 = 0
-    result = []
-    for i in range(0,len(nums)):
-    	start,end = i,len(nums -1 )
+	"""
+	:type nums: List[int]
+	:rtype: List[List[int]]
+	"""
+	nums.sort()
+	curr_2 = 0
+	curr_3 = 0
+	result = []
+	if len(nums) <3 or (sum(nums) == 3 and len(nums) == 3):
+	    return []
 
-    	while (start <  end):
-	    	curr_2 = nums[end] + nums[start] + nums[i]
-	    	if curr_2 == 0:
-	    		print(nums[end],nums[start] ,nums[i])
-	    		break
-	    	elif curr_2 < 0:
-	    		start +=1
-	    	else:
-	    		end -=1
+	for i in range(0,len(nums)):
+	    start,end = i,len(nums )-1 
+	    if nums[i] == nums[i-1] :
+	        
+	        continue
+	    else:
+	        while (start <  end):
+	            curr_2 = nums[end] + nums[start] + nums[i]
+	            print(nums[end],nums[start] ,nums[i], curr_2)
+	            if curr_2 == 0:
+	            	print("fh")
+	            	result.append([nums[end],nums[start] ,nums[i]])
+	            	print
+	            	end -=1
+	            elif curr_2 < 0:
+	                start +=1
+	            elif curr_2 > 0:
+	                end -=1
+	print (result)
+threeSum(nums)
+#threeSum([-25,-10,-7,-3,2,4,8,10])
